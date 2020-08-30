2e6d7c1538fdb13f0225ed64406e3bc40a65e063
Isale <isale.bryan@gmail.com>
2020-08-30 16:47:34 +0300

    Load weather on Dom load

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-
 // Initiate weather object
 
 const weather = new Weather('london','uk');
 
-weather.getWeather()
+// Get weather on DOM Load
+
+document.addEventListener('DOMContentLoaded', getWeather)
+
+weather.changeLocation('nakuru');
+
+function getWeather() {
+  weather.getWeather()
 .then(result => console.log(result))
 .catch(error=> console.log(error));
+}
+
