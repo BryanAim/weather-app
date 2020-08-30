578e1cf0cc393f8dfd22ffcee4e2046dbb9251d0
Isale <isale.bryan@gmail.com>
2020-08-30 15:17:20 +0300

    Create Waether class that calls api

---
+++ weather.js
@@ -0,0 +1,26 @@
+class Weather {
+  constructor(city,state) {
+    this.apiKey = 'fc5d1d3640c6d5841191dbe13c529396';
+    this.city =  city;
+    this .state = state;
+  }
+
+    // Fetch Weather from OpenWeather API
+
+    async getWeather() {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q={${this.city}}&appid={${this.apiKey}}`);
+
+      const responseData = await response.json();
+
+      return responseData.main;
+    }
+    
+
+  // Change Weather location 
+
+  changeLocation(city, state) {
+    this.city = city;
+    this.state = state;
+  }
+
+}
